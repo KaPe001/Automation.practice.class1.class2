ddbf4113f9ed550881404f50d5790c86fc35d33f
kpetkowska <kingapetkowska@gmail.com>
2021-11-08 12:41:12 +0100

    token generator using ASCII table and banning user from entering String instead of the integer

--- src/main/java/class2/TokenGeneratorArray.java
+++ src/main/java/class2/TokenGeneratorArray.java
@@ -1,5 +1,6 @@
 package class2;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,33 +9,35 @@ public class TokenGeneratorArray {
 
         Scanner sc = new Scanner(System.in);
 
-        String digits = "0123456789";
-        String specialChars = "~!@#$%^&*()=+{}[]|?ABCDEFGHIJKLMNOPRSTUVWYZabcdfghijklmnoprstuvwyz"
-                + digits;
+        char asciTable[] = {33, 35, 36, 37, 38, 40, 41, 43, 49, 50, 55, 57, 64, 71, 72, 73, 78, 83, 85, 98, 99,
+                100, 102, 104, 105, 107, 113, 114, 116, 119, 120};
         Random rnd = new Random();
 
-        System.out.println("Enter the size of the token");
-        int userChoiceOfLength = sc.nextInt();
+        try {
+            System.out.println("Enter the size of the token");
+            int userChoiceOfLength = sc.nextInt();
+            char[] buf = new char[userChoiceOfLength];
 
-        char[] symbols = specialChars.toCharArray();
-        char[] buf = new char[userChoiceOfLength];
-
-        switch (userChoiceOfLength) {
-            case 15:
-                for (int i = 0; i < buf.length; i++)
-                    System.out.print(buf[i] = symbols[rnd.nextInt(symbols.length)]);
-                break;
-            case 10:
-                for (int i = 0; i < buf.length; i++)
-                    System.out.print(buf[i] = symbols[rnd.nextInt(symbols.length)]);
-                break;
-            case 5:
-                for (int i = 0; i < buf.length; i++)
-                    System.out.print(buf[i] = symbols[rnd.nextInt(symbols.length)]);
-                break;
-            default:
-                System.out.println("Size not available");
+            switch (userChoiceOfLength) {
+                case 15:
+                    for (int i = 0; i < buf.length; i++)
+                        System.out.print(buf[i] = asciTable[rnd.nextInt(asciTable.length)]);
+                    break;
+                case 10:
+                    for (int i = 0; i < buf.length; i++)
+                        System.out.print(buf[i] = asciTable[rnd.nextInt(asciTable.length)]);
+                    break;
+                case 5:
+                    for (int i = 0; i < buf.length; i++)
+                        System.out.print(buf[i] = asciTable[rnd.nextInt(asciTable.length)]);
+                    break;
+                default:
+                    System.out.println("Size not available");
+            }
+            return new String(buf);
+        } catch (InputMismatchException e) {
+            System.out.println("Input is not an integer");
         }
-        return new String(buf);
+        return null;
     }
 }
